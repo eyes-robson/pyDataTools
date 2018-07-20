647642850e8af7ff50792136a25b7cc243840977
isrobson <isrobson@email.unc.edu>
2018-07-20 22:41:21 +0000

    add LarsCV to linear feature selection

--- dataSci/featureSelection/linear_selection.py
+++ dataSci/featureSelection/linear_selection.py
@@ -7,3 +7,13 @@ def fs_lasso_cv(X,y,feat_list, n_alphas=1000, cv=10, tol=0.00001, max_iter=10000
     lcv = LassoCV(n_jobs = max(1, cpu_count()-1), n_alphas=n_alphas, cv=cv, tol=tol, max_iter=max_iter)
     lcv.fit(X,y)
     return list(compress(feat_list, lcv.coef_))
+
+def fs_lars_cv(X,y,feat_list, n_alphas=1000, cv=10, max_iter=1000):
+    from multiprocessing import cpu_count
+    from sklearn.linear_model import LarsCV
+    import numpy as np
+    from itertools import compress 
+
+    lcv = LarsCV(n_jobs = max(1, cpu_count()-1), max_n_alphas=n_alphas, cv=cv, max_iter=max_iter)
+    lcv.fit(X,y)
+    return list(compress(feat_list, lcv.coef_))

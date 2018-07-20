5ffb1516a0dd34f5f6d61be124433a81b2688490
isrobson <isrobson@email.unc.edu>
2018-07-20 23:34:51 +0000

    add hard_shrink option to fs_lasso and fs_lars

--- dataSci/featureSelection/linear_selection.py
+++ dataSci/featureSelection/linear_selection.py
@@ -3,16 +3,28 @@ import numpy as np
 import itertools as it
 from sklearn import linear_model
 
-def fs_lasso_cv(X,y,feat_list, n_alphas=1000, cv=10, tol=0.00001, max_iter=10000):
+def fs_lasso_cv(X,y,feat_list, n_alphas=1000, cv=10, tol=0.00001, max_iter=10000, hard_shrink=None):
     '''Wrapper function to build a LassoCV model from sklearn and return important features'''
     
     lcv = linear_model.LassoCV(n_jobs = max(1, mp.cpu_count()-1), n_alphas=n_alphas, cv=cv, tol=tol, max_iter=max_iter)
-    lcv.fit(X,y)
-    return list(it.compress(feat_list, lcv.coef_))
+    coefs = lcv.fit(X,y).coef_
 
-def fs_lars_cv(X,y,feat_list, n_alphas=1000, cv=10, max_iter=1000):
+    # force shrinkage to zero if hard_shrink is provided
+    if hard_shrink is not None: np.place(coefs, coefs < hard_shrink, 0)
+    
+    selected_feats = list(it.compress(feat_list, coefs))
+    
+    return selected_feats
+
+def fs_lars_cv(X,y,feat_list, n_alphas=1000, cv=10, max_iter=1000, hard_shrink=None):
     '''Wrapper function to build a LarsCV model from sklearn and return important features'''
     
     lcv = linear_model.LarsCV(n_jobs = max(1, mp.cpu_count()-1), max_n_alphas=n_alphas, cv=cv, max_iter=max_iter)
-    lcv.fit(X,y)
-    return list(it.compress(feat_list, lcv.coef_))
+    coefs = lcv.fit(X,y).coef_
+
+    # force shrinkage to zero if hard_shrink is provided
+    if hard_shrink is not None: np.place(coefs, coefs < hard_shrink, 0)
+    
+    selected_feats = list(it.compress(feat_list, coefs))
+    
+    return selected_feats

c564b5856b13beb25c761a70cd3e227a3ffac3c9
isrobson <isrobson@email.unc.edu>
2018-07-21 03:07:28 +0000

    add bootstrap generator in feature engineering

--- dataSci/featureEngineering/interaction.py
+++ dataSci/featureEngineering/interaction.py
@@ -2,6 +2,7 @@ import multiprocessing as mp
 import numpy as np
 import itertools as it
 from sklearn import preprocessing as pre
+from sklearn import utils
 
 def fe_polynomial(X, feat_list, deg=2, interaction_only=False, include_bias=False, include_lower=True):
     '''Wrapper for sklearn.preprocessing PolynomialFeatures to create higher order interaction terms. Also gives names.'''
@@ -20,4 +21,18 @@ def fe_polynomial(X, feat_list, deg=2, interaction_only=False, include_bias=Fals
         feat_list_out = feat_list_out[len_feat_list_lower:]
         
     return (data_out,feat_list_out)
+
+def bootstrap(X,y,model,n_samples = None, allow_repeats = True, random_seed = None):
+    '''Wrapper for sklearn.utils resample to generate a bootstrapped dataset, both X and y'''
+
+    y_hats = model.predict(X)
+    resids = y - y_hats
+    
+    # shuffle the residuals and draw matching samples from X and y_hats
+    X_new, y_hats_new, resids_new = utils.resample(X,y_hats,utils.shuffle(resids),n_samples=n_samples,random_state=random_seed,replace=allow_repeats)
+    y_new = y_hats_new + resids_new
+    
+    return(X_new, y_new)
+    
+    
     
